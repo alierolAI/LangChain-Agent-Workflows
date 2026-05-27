9c48d0737ea8bc309379a475a123df06da1d1e4f
Ali Erol <erolali4819@gmail.com>
2026-05-27 06:30:36 +0300

    Fix LangChain message history implementation

--- MessagingHistory.py
+++ MessagingHistory.py
@@ -1,3 +1,6 @@
+from distutils.command.config import config
+from urllib import response
+
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage
@@ -5,9 +8,28 @@ from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMess
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
-
+from SimpleMessage import model
 
 load_dotenv()
+
+
+store={}
+def get_sesion_history(session_id : str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id]= InMemoryChatMessageHistory()
+    return store[session_id]
+
+promt=ChatPromptTemplate.from_messages(
+    [
+        ("system","you are a helpful assistant. Answer all questions bast of your ability"),
+        MessagesPlaceholder(variable_name="messages")
+    ]
+)
+
+chain= promt | model
+config={"configurable" : {"session_id" : "abcde123"}}
+with_message_history = RunnableWithMessageHistory(chain, get_sesion_history)
+
 
 
 
@@ -16,6 +38,13 @@ model = ChatOpenAI(model="gpt-4o-mini")
 
  
 if __name__ == '__main__':
-    message=HumanMessage(content="hello my name is ali")
-    response=model.invoke([message])
-    print(response.content)
+    for sayi in range(10):
+        user_input = input(">")
+        response= with_message_history.invoke(
+            [
+                HumanMessage(content=user_input),
+            ],
+            config=config,
+        )
+        print(response.content)
+
